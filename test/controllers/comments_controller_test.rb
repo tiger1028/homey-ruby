16600d28f045686e1b3cfe1671bbd72bcd6af3cd
tiger1028 <gentleman.tiger1028@gmail.com>
2023-06-13 14:16:22 +0200

    feat: add test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :comments
+
+  test "should create comment" do
+    assert_difference('Comment.count') do
+      post comments_url, params: { comment: comments(:one).comment }
+      assert_redirected_to project_index_path
+    end
+  end
 end
--- test/controllers/status_logs_controller_test.rb
+++ test/controllers/status_logs_controller_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class StatusLogsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :status_logs
+
+  test "should change status" do
+    assert_difference('StatusLog.count') do
+      post status_logs_url, params: { status: status_logs(:one).status }
+      assert_redirected_to project_index_path
+    end
+  end
 end
